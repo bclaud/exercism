02f29ab965b9cc1baaad75a573b7b97d8d275a6b
Bruno Claudino <bruarrais@gmail.com>
2023-01-16 22:14:31 -0300

    nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "exercism shell";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    devshell-flake = { url = "github:numtide/devshell"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell-flake }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [devshell-flake.overlay];
+        };
+      in
+
+      with pkgs;
+      { devShell = callPackage ./shell.nix { inherit devshell-flake; }; }
+    );
+}
---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs  }:
+
+with pkgs;
+
+let
+  projectName = "execism";
+
+  exercismPython = python311.withPackages (p: with p; [pytest pytest-cache pytest-subtests pytest-pylint]);
+in
+
+mkShell {
+  name = "${projectName}-shell";
+  
+  packages = [
+    glibcLocalesUtf8
+    elixir
+    exercism
+    exercismPython
+  ];
+
+  LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else ""; 
+}

cac1be8920c998aa485bd8a2f6693fdc54b36539
Bruno Claudino <bruarrais@gmail.com>
2023-02-11 10:31:28 -0300

    add fsharp

--- flake.nix
+++ flake.nix
@@ -2,22 +2,25 @@
   description = "exercism shell";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
-    devshell-flake = { url = "github:numtide/devshell"; };
+    nixpkgs = {url = "github:NixOS/nixpkgs/nixpkgs-unstable";};
+    flake-utils = {url = "github:numtide/flake-utils";};
+    devshell-flake = {url = "github:numtide/devshell";};
   };
 
-  outputs = { self, nixpkgs, flake-utils, devshell-flake }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell-flake,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         inherit (nixpkgs.lib) optional;
         pkgs = import nixpkgs {
           inherit system;
           # overlays = [devshell-flake.overlay];
         };
       in
-
-      with pkgs;
-      { devShell = callPackage ./shell.nix { }; }
+        with pkgs; rec {devShell = callPackage ./shell.nix { };}
     );
 }
--- shell.nix
+++ shell.nix
@@ -6,6 +6,8 @@ let
   projectName = "execism";
 
   exercismPython = python3.withPackages (p: with p; [pytest pytest-cache pytest-subtests pytest-pylint]);
+
+  fSharp = [exercism dotnet-sdk_7 dotnet-runtime_7];
 in
 
 mkShell {
@@ -16,6 +18,7 @@ mkShell {
     elixir
     exercism
     exercismPython
+    fSharp
   ];
 
   LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else ""; 
